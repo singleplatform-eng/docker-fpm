9b08d32aaa332405392644845cfeb4b58926fcd6
Colin Hoglund <colinhoglund@gmail.com>
2016-09-03 12:41:55 -0400

    update travis script

---
+++ scripts/build_changes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -v
+
+# get all Dockerfiles that have changed
+CHANGED_IMAGES=$(git diff --name-only HEAD | grep Dockerfile)
+
+for i in $CHANGED_IMAGES; do
+  fullpath=$(git rev-parse --show-toplevel)/$i
+  # replace / with - to get the docker tag value
+  tag=$(dirname $i | sed 's/^fpm\///;s/\//-/g')
+
+  # build each newly changed Dockerfile
+  echo "Building colinhoglund/fpm:${tag} image from ${i}:"
+  cd $(dirname ${fullpath})
+  docker build -t colinhoglund/fpm:${tag} .
+  cd - &> /dev/null
+done
