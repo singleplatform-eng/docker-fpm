10b49f60236b9058c5d158c71d32a341b0bb982c
Colin Hoglund <colinhoglund@gmail.com>
2016-09-04 21:17:23 -0400

    allow build script to update travis build matrix

--- build.sh
+++ build.sh
@@ -3,39 +3,74 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
 function usage {
-  echo "Usage: $0 -a|-c|dockerfile
+    echo "Usage: $0 -a|-c|-tdockerfile
 
 Options:
-  -a, --all       Build all Docker images
-  -c, --changed   Build Docker images that have changed since previous git revision"
+    -a, --all       Build all Docker images
+    -c, --changed   Build Docker images that have changed since previous git revision
+    -h, --help      Show this help message
+    -t, --travis    Update .travis.yml build matrix"
+}
 
-  exit 1
+function update_travis_build_matrix {
+    travis_file=$GIT_ROOT/.travis.yml
+    touch $travis_file
+
+    # .travis.yml pre-config
+    echo 'sudo: required
+
+# skip language installation steps for docker builds
+install: true
+
+services:
+    - docker
+
+env:' > $travis_file
+
+    # append images/scripts to test
+    travis_tests=$(git ls-files | egrep 'Dockerfile|build_scripts')
+    for t in $travis_tests; do
+        echo "    - TEST: ${t}" >> $travis_file
+    done
+
+    # .travis.yml post-config
+    printf "\nscript: ./tests.sh\n" >> $travis_file
 }
 
 # only accept one argument
 if [ -n "$2" ]; then
-  usage
+    usage
+    exit 1
 fi
 
 # get images to build based on command line flag
 case $1 in
-  -a|--all)
-    # get all Dockerfiles
-    IMAGES=$(git ls-files *Dockerfile)
-    ;;
-  -c|--changed)
-    # get all Dockerfiles that have changed since previous revision
-    IMAGES=$(git diff --diff-filter=ACMR --name-only HEAD^ *Dockerfile)
-    ;;
-  *)
-    # check that dockerfile exists
-    if [ -n "$1" ] && [ -f ${GIT_ROOT}/${1} ]; then
-      # set images to specified docker file
-      IMAGES=$1
-    else
-      usage
-    fi
-    ;;
+    -a|--all)
+        # get all Dockerfiles
+        IMAGES=$(git ls-files *Dockerfile)
+        ;;
+    -c|--changed)
+        # get all Dockerfiles that have changed since previous revision
+        IMAGES=$(git diff --diff-filter=ACMR --name-only HEAD^ *Dockerfile)
+        ;;
+    -h|--help)
+        usage
+        exit 0
+        ;;
+    -t|--travis)
+        update_travis_build_matrix
+        exit 0
+        ;;
+    *)
+        # check that dockerfile exists
+        if [ -n "$1" ] && [ -f ${GIT_ROOT}/${1} ]; then
+            # set images to specified docker file
+            IMAGES=$1
+        else
+            usage
+            exit 1
+        fi
+        ;;
 esac
 
 for i in $IMAGES; do
